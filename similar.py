322b53e47bafab75ceba6e479251eab561ed58d3
Levi Sällberg <levi.sallberg@fcg.se>
2022-04-14 13:21:06 +0200

    updates to similar model

--- similar.py
+++ similar.py
@@ -3,30 +3,40 @@ import json
 import numpy as np
 
 
+class Article:
+    def __init__(self, id, title, description, text):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.text = text
+        self.similar_ids = []
+
+    def get_content(self):
+        result = []
+        if self.title is not None:
+            result.append(self.title)
+        if self.description is not None:
+            result.append(self.description)
+        if self.text is not None:
+            result.append(self.text)
+        if len(result) > 0:
+            return '. '.join(result)
+        else:
+            return ''
+
+
 def read_input(path):
     f = open(path)
     articles = json.load(f)
-    texts = []
-    for article in articles:
-        result = []
-
-        article_title = article['title']
-        if article_title is not None:
-            result.append(article_title)
-
-        article_description = article['description']
-        if article_description is not None:
-            result.append(article_description)
-
-        article_text = article['text']
-        if article_text is not None:
-            result.append(article_text)
-
-        if len(result) > 0:
-            texts.append('. '.join(result))
-
     f.close()
-    return texts
+
+    result = []
+    for article in articles:
+        result.append(
+            Article(id=article['id'], title=article['title'], description=article['description'], text=article['text'])
+        )
+
+    return result
 
 
 def get_similar_index(row, limit):
@@ -35,34 +45,32 @@ def get_similar_index(row, limit):
 
 def calculate_similarity(filename):
     input_path = './resources/{}.json'.format(filename)
-    output_path = './resources/{}-bert.pkl'.format(filename)
-    texts = read_input(input_path)
+    score_limit = 0.5
+
+    articles = read_input(input_path)
+    texts = []
+    for article in articles:
+        texts.append(article.get_content())
+
+    # Compute and set similar articles
     tfidf = TfidfVectorizer().fit_transform(texts)
     pairwise_similarity = (tfidf * tfidf.T).toarray()
     np.fill_diagonal(pairwise_similarity, np.nan)
 
-    # Get similar texts
-    index = 4
-    similar_indices = get_similar_index(pairwise_similarity[index], 0.5)
+    for article_idx, article in enumerate(articles):
+        similarities = pairwise_similarity[article_idx]
+        similar_indices = np.argwhere(similarities >= score_limit).tolist()
+        similar_articles = []
+        for similar_index in similar_indices:
+            similar_articles.append(articles[similar_index[0]])
+        article.similar_ids = similar_articles
 
-    similar = []
-
-    for similar_index in similar_indices:
-        similar.append(texts[similar_index[0]])
-
-    return texts[index], similar
+    return articles
 
 
 if __name__ == "__main__":
-    file = '100-fi'
-    evaluated_text, similar_texts = calculate_similarity(file)
-
-    print("Evaluated text: ")
-    print(evaluated_text)
-
-    print("Similar texts: ")
-    for text in similar_texts:
-        print("\nSimilar: ")
-        print(text)
+    file = '1000'
+    articles = calculate_similarity(file)
+    print(articles)
 
 
