85e6abba3e80009f5f7a789393c7aaac7cd5aa74
Levi Sällberg <levi.sallberg@fcg.se>
2022-04-28 14:40:59 +0200

    predict

--- fetch_dev.sql
+++ fetch_dev.sql
@@ -19,33 +19,35 @@ limit 100;
 
 
 -- With tags finansinspektionen
-select a.id, a.title, a.description, a.text, json_agg(at.value) as tags
+select a.id, a.link, a.title, a.description, a.text, coalesce(tags.tags, '[]') as tags
 from articles a
-inner join article_tag_association ata on a.id = ata.article_id
-inner join article_tags at on ata.tag_id = at.id
-where
-      text is not null AND
-      a.publisher_id = '9a6fe83f-ed66-420e-89b8-2c855395afa8'AND
-      at.value in (
-                   'Penningtvätt',
-                   'Tjänstepensionsföretag',
-                   'Hållbarhet',
-                   'Sanktioner',
-                   'Försäkringsföretag',
-                   'Värdepappersinstitut',
-                   'Bank och kredit',
-                   'Pensionsstiftelser',
-                   'Försäkring',
-                   'Bank',
-                   'Försäkringsförmedlare'
+left join (
+    select ata.article_id, json_agg(at.value) as tags
+    from article_tags at
+             inner join article_tag_association ata on at.id = ata.tag_id
+    where at.value in (
+                       'Penningtvätt',
+                       'Tjänstepensionsföretag',
+                       'Hållbarhet',
+                       'Sanktioner',
+                       'Försäkringsföretag',
+                       'Värdepappersinstitut',
+                       'Bank och kredit',
+                       'Pensionsstiftelser',
+                       'Försäkring',
+                       'Bank',
+                       'Försäkringsförmedlare'
         )
-group by a.id;
+    group by ata.article_id
+) tags on tags.article_id = a.id
+where a.text is not null AND a.publisher_id = '9a6fe83f-ed66-420e-89b8-2c855395afa8';
 
 
-select a.id, a.title, a.description, a.text
+select a.id, a.link, a.title, a.description, a.text
 from articles a
 where text is not null and a.publisher_id IN (
-    '9a6fe83f-ed66-420e-89b8-2c855395afa8',
-    'f317d418-4e83-43fd-9b07-72aa28a795c3'
+                                              'da0e66b1-b2f9-4349-9ecd-2868caee58ae',
+                                              'f317d418-4e83-43fd-9b07-72aa28a795c3'
     );
+
 
